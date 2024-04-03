a41d012129e15b0f7f188fdefee1fbe1e1ea96e7
Akram3104 <abdulakram2002@gmail.com>
2024-04-03 23:55:41 +0530

    testing subtraction
    testing without subtraction function

--- test_calculator.py
+++ test_calculator.py
@@ -1,11 +1,16 @@
 import unittest
-from calculator import addition
+from calculator import addition,subtraction
 
 class TestCalculator(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(addition(5, 3), 8)
         self.assertEqual(addition(-5, 3), -2)
         self.assertEqual(addition(0, 0), 0)
+    def test_subtraction(self):
+        self.assertEqual(subtraction(5, 3), 2)
+        self.assertEqual(subtraction(5, -3), 8)
+        self.assertEqual(subtraction(0, 0), 0)
+
 
        
 if __name__ == "__main__":
